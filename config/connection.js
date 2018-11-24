56a99cd890303bb511cbf0762cf9067d66bcc30a
jonabari <barcelo.jonathan@gmail.com>
2018-11-24 08:21:22 -0600

    jawsdb for heroku

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,21 @@
 // Set up MySQL connection.
 const mysql = require('mysql')
 
-const connection = mysql.createConnection({
-    host: 'localhost',
-    port: 8889,
-    user: 'root',
-    password: 'root',
-    database: 'burgers_db'
-})
+if (process.env.JAWSDB_URL) {
+    // Database is JawsDB on Heroku
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    // Database is local
+    connection = mysql.createConnection({
+        host: 'localhost',
+        port: 8889,
+        user: 'root',
+        password: 'root',
+        database: 'burgers_db'
+    })
+};
+
+mysql://bcvxbznufa0ozf0z:zvykz24gm48iaktw@bmsyhziszmhf61g1.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/jdzozpdnahjaky3n
 
 // Make connection.
 connection.connect(function (err) {
