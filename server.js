393cd5902d532d2d948273272d3d5cc86dac4264
jonabari <barcelo.jonathan@gmail.com>
2018-11-23 21:40:35 -0600

    Finished app c:

--- server.js
+++ server.js
@@ -1,7 +1,5 @@
 const express = require('express')
-
 const PORT = process.env.PORT || 8080
-
 const app = express()
 
 // Serve static content for the app from the "public" directory in the application directory.
