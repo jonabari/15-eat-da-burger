314dea5978fbf62b6a96bbf67e7b3379f9a46877
jonabari <barcelo.jonathan@gmail.com>
2018-11-23 19:45:32 -0600

    Working app, missing styling

---
+++ db/seeds.sql
@@ -0,0 +1,24 @@
+INSERT INTO burgers
+    (name, devoured)
+VALUES
+    ('Cheese', true);
+INSERT INTO burgers
+    (name, devoured)
+VALUES
+    ('Quarter Pounder', true);
+INSERT INTO burgers
+    (name, devoured)
+VALUES
+    ('Goat Cheese & Portobello', false);
+INSERT INTO burgers
+    (name, devoured)
+VALUES
+    ('Chicken', false);
+INSERT INTO burgers
+    (name, devoured)
+VALUES
+    ('Carnitas', false);
+INSERT INTO burgers
+    (name, devoured)
+VALUES
+    ('Al Pastor', false);
